93d92c073658b8c020ad457f4c009f73a5a5577e
bedirhhanaydin <bedirhhanaydin@gmail.com>
2022-04-08 19:20:07 +0300

    Initial Commit

---
+++ lib/model/kitap.dart
@@ -0,0 +1,13 @@
+class Kitap {
+  String image;
+  String kitapAdi;
+  String yayinEvi;
+  String yazar;
+  String fiyat;
+  Kitap(
+      {required this.image,
+      required this.kitapAdi,
+      required this.yayinEvi,
+      required this.yazar,
+      required this.fiyat});
+}
